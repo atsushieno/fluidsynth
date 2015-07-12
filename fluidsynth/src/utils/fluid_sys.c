cd8918e243599792a7db8beeedf5b9f55b51072d
Atsushi Eno <atsushieno@gmail.com>
2015-07-12 17:11:37 +0900

    ... do I need __ before android_log_write?

--- fluidsynth/src/utils/fluid_sys.c
+++ fluidsynth/src/utils/fluid_sys.c
@@ -156,28 +156,28 @@ fluid_default_log_function(int level, char* message, void* data)
   switch (level) {
   case FLUID_PANIC:
 #ifdef ANDROID
-    android_log_write(ANDROID_LOG_FATAL, fluid_libname, message);
+    __android_log_write(ANDROID_LOG_FATAL, fluid_libname, message);
 #else
     FLUID_FPRINTF(out, "%s: panic: %s\n", fluid_libname, message);
 #endif
     break;
   case FLUID_ERR:
 #ifdef ANDROID
-    android_log_write(ANDROID_LOG_ERROR, fluid_libname, message);
+    __android_log_write(ANDROID_LOG_ERROR, fluid_libname, message);
 #else
     FLUID_FPRINTF(out, "%s: error: %s\n", fluid_libname, message);
 #endif
     break;
   case FLUID_WARN:
 #ifdef ANDROID
-    android_log_write(ANDROID_LOG_WARN, fluid_libname, message);
+    __android_log_write(ANDROID_LOG_WARN, fluid_libname, message);
 #else
     FLUID_FPRINTF(out, "%s: warning: %s\n", fluid_libname, message);
 #endif
     break;
   case FLUID_INFO:
 #ifdef ANDROID
-    android_log_write(ANDROID_LOG_INFO, fluid_libname, message);
+    __android_log_write(ANDROID_LOG_INFO, fluid_libname, message);
 #else
     FLUID_FPRINTF(out, "%s: %s\n", fluid_libname, message);
 #endif
@@ -185,7 +185,7 @@ fluid_default_log_function(int level, char* message, void* data)
   case FLUID_DBG:
 #if DEBUG
 #ifdef ANDROID
-    android_log_write(ANDROID_LOG_DEBUG, fluid_libname, message);
+    __android_log_write(ANDROID_LOG_DEBUG, fluid_libname, message);
 #else
     FLUID_FPRINTF(out, "%s: debug: %s\n", fluid_libname, message);
 #endif
@@ -193,7 +193,7 @@ fluid_default_log_function(int level, char* message, void* data)
     break;
   default:
 #ifdef ANDROID
-    android_log_write(ANDROID_LOG_VERBOSE, fluid_libname, message);
+    __android_log_write(ANDROID_LOG_VERBOSE, fluid_libname, message);
 #else
     FLUID_FPRINTF(out, "%s: %s\n", fluid_libname, message);
 #endif
